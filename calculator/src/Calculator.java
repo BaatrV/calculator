a6b8f89afe5bd1e466d45bc5c95c8c092052656f
BaatrV <135658972+BaatrV@users.noreply.github.com>
2023-06-06 14:30:23 +0300

    Update Calculator.java

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -3,42 +3,49 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double num1, num2, result;
-        char operator;
+        System.out.print("Введите выражение в формате 'число операция число' от 1 до 10 включительно: ");
+        String input = scanner.nextLine();
 
-        System.out.print("Введите первое число: ");
-        num1 = scanner.nextDouble();
 
-        System.out.print("Введите оператор (+, -, *, /): ");
-        operator = scanner.next().charAt(0);
+        String[] parts = input.split(" ");
+        if (parts.length != 3) {
+            System.out.println("Ошибка: некорректный формат выражения.");
+            return;
+        }
 
-        System.out.print("Введите второе число: ");
-        num2 = scanner.nextDouble();
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.println(num1 + " + " + num2 + " = " + result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println(num1 + " - " + num2 + " = " + result);
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println(num1 + " * " + num2 + " = " + result);
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("Ошибка: деление на ноль");
-                } else {
-                    result = num1 / num2;
-                    System.out.println(num1 + " / " + num2 + " = " + result);
+        int number1, number2;
+        try {
+            number1 = Integer.parseInt(parts[0]);
+            number2 = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка: некорректное числовое значение.");
+            return;
+        }
+
+
+        int result;
+        if (number1 > 10 || number2 > 10) {
+            System.out.println("Введенное число больше 10");
+        } else {
+            switch (parts[1]) {
+                case "+" -> result = number1 + number2;
+                case "-" -> result = number1 - number2;
+                case "*" -> result = number1 * number2;
+                case "/" -> {
+                    if (number2 == 0) {
+                        System.out.println("Ошибка: деление на ноль.");
+                        return;
+                    }
+                    result = number1 / number2;
                 }
-                break;
-            default:
-                System.out.println("Ошибка: неверный оператор");
-                break;
+                default -> {
+                    System.out.println("Ошибка: некорректная операция.");
+                    return;
+                }
+            }
+
+            System.out.println("Результат: " + result);
         }
     }
 }
